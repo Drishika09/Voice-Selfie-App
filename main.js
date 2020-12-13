ff4846536b68863104684f6c4f26e7484423b7b8
Drishika09 <74666535+Drishika09@users.noreply.github.com>
2020-12-13 16:32:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+recognition.onresult=function(event){
+    console.log(event);
+    var Content=event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML=Content;
+    console.log(Content);
+    if(Content=="take my selfie"){
+    speak();
+    }
+    
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data="taking selfie in 5 seconds";
+    console.log("speaking");
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_selfie();
+        save();
+    },5000);
+}
+camera=document.getElementById("camera");
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
+function take_selfie(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_img" src="'+data_uri+'">';
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    img=document.getElementById("selfie_img").src;
+    link.href=img;
+    link.click();
+}
